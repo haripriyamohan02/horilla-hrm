89f2a37e1a7cf2dad150649d82dbb8cc91b96827
harini <harinisree.s@tringapps.net>
2025-06-24 15:24:49 +0530

    clock_in-issue-fix

--- attendance/views/clock_in_out.py
+++ attendance/views/clock_in_out.py
@@ -138,27 +138,21 @@ def clock_in_attendance_and_activity(
     """
 
     # attendance activity create
-    activity = AttendanceActivity.objects.filter(
+    open_activity = AttendanceActivity.objects.filter(
         employee_id=employee,
         attendance_date=attendance_date,
-        clock_in_date=date_today,
-        shift_day=day,
         clock_out=None,
     ).first()
 
-    if activity and not activity.clock_out:
-        activity.clock_out = in_datetime
-        activity.clock_out_date = date_today
-        activity.save()
-
-    new_activity = AttendanceActivity.objects.create(
-        employee_id=employee,
-        attendance_date=attendance_date,
-        clock_in_date=date_today,
-        shift_day=day,
-        clock_in=in_datetime,
-        in_datetime=in_datetime,
-    )
+    if not open_activity:
+        AttendanceActivity.objects.create(
+            employee_id=employee,
+            attendance_date=attendance_date,
+            clock_in_date=date_today,
+            shift_day=day,
+            clock_in=in_datetime,
+            in_datetime=in_datetime,
+        )
     # create attendance if not exist
     attendance = Attendance.objects.filter(
         employee_id=employee, attendance_date=attendance_date
